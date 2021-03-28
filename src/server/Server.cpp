7c0e580ebec3b3f6e14f1906c0626da466da5ce3
Bogdan <pop.bogdan.pvag@gmail.com>
2021-03-28 14:11:58 +0300

    feat(rest-server): ZoomCharts reading JSON

--- src/server/Server.cpp
+++ src/server/Server.cpp
@@ -4,15 +4,16 @@
    Example of a REST endpoint with routing
 */
 
-#include <algorithm>
-
+#include <pistache/endpoint.h>
 #include <pistache/http.h>
 #include <pistache/router.h>
-#include <pistache/endpoint.h>
+
+#include <algorithm>
 
 #include "rapidjson/document.h"
-#include "rapidjson/writer.h"
 #include "rapidjson/stringbuffer.h"
+#include "rapidjson/writer.h"
+#include "rapidjson/prettywriter.h"
 
 using namespace std;
 using namespace Pistache;
@@ -22,7 +23,7 @@ void printCookies(const Http::Request& req) {
     auto cookies = req.cookies();
     std::cout << "Cookies: [" << std::endl;
     const std::string indent(4, ' ');
-    for (const auto& c: cookies) {
+    for (const auto& c : cookies) {
         std::cout << indent << c.name << " = " << c.value << std::endl;
     }
     std::cout << "]" << std::endl;
@@ -32,38 +33,145 @@ namespace Generic {
 
 void handleReady(const Rest::Request&, Http::ResponseWriter response) {
     Document doc;
-    doc.SetObject();
 
-    Value v;
-    v.SetInt(10);
-    Value b(true);
-    Value s;
-    s.SetString("Ciccio", strlen("Ciccio"), doc.GetAllocator());
+    // Create JSON with objects
+    /*     doc.SetObject();
 
-    doc.AddMember("Valore", v, doc.GetAllocator());
-    doc.AddMember("Booleano", b, doc.GetAllocator());
-    doc.AddMember("Sexy", s, doc.GetAllocator());
+        Value v;
+        v.SetInt(10);
+        Value b(true);
+        Value s;
+        s.SetString("Ciccio", strlen("Ciccio"), doc.GetAllocator());
 
+        doc.AddMember("Valore", v, doc.GetAllocator());
+        doc.AddMember("Booleano", b, doc.GetAllocator());
+        doc.AddMember("Sexy", s, doc.GetAllocator());
+     */
 
-    StringBuffer buf;
-    Writer<StringBuffer> writer(buf);
-    doc.Accept(writer);
+    StringBuffer         buf;
+    PrettyWriter<StringBuffer> writer(buf);
+    /*     doc.Accept(writer);
+     */
+
+    writer.StartObject();
+
+    writer.Key("nodes");
+    writer.StartArray();
+
+    writer.StartObject();
+    writer.Key("id");
+    writer.String("n1");
+    writer.Key("loaded");
+    writer.Bool(true);
+    writer.Key("style");
+    writer.StartObject();
+    writer.Key("fillColor");
+    writer.String("rgba(236,46,46,0.8)");
+    writer.Key("label");
+    writer.String("Node1");
+    writer.EndObject();
+    writer.EndObject();
+
+    writer.StartObject();
+    writer.Key("id");
+    writer.String("n2");
+    writer.Key("loaded");
+    writer.Bool(true);
+    writer.Key("style");
+    writer.StartObject();
+    writer.Key("fillColor");
+    writer.String("rgba(47,195,47,0.8)");
+    writer.Key("label");
+    writer.String("Node2");
+    writer.EndObject();
+    writer.EndObject();
+
+    writer.StartObject();
+    writer.Key("id");
+    writer.String("n3");
+    writer.Key("loaded");
+    writer.Bool(true);
+    writer.Key("style");
+    writer.StartObject();
+    writer.Key("fillColor");
+    writer.String("rgba(28,124,213,0.8)");
+    writer.Key("label");
+    writer.String("Node3");
+    writer.EndObject();
+    writer.EndObject();
+
+    writer.EndArray();
+
+    writer.Key("links");
+    writer.StartArray();
+
+    writer.StartObject();
+    writer.Key("id");
+    writer.String("l1");
+    writer.Key("from");
+    writer.String("n1");
+    writer.Key("to");
+    writer.String("n2");
+    writer.Key("style");
+    writer.StartObject();
+    writer.Key("fillColor");
+    writer.String("rgba(236,46,46,1)");
+    writer.Key("toDecoration");
+    writer.String("arrow");
+    writer.EndObject();
+    writer.EndObject();
+
+    writer.StartObject();
+    writer.Key("id");
+    writer.String("l2");
+    writer.Key("from");
+    writer.String("n2");
+    writer.Key("to");
+    writer.String("n3");
+    writer.Key("style");
+    writer.StartObject();
+    writer.Key("fillColor");
+    writer.String("rgba(47,195,47,1)");
+    writer.Key("toDecoration");
+    writer.String("arrow");
+    writer.EndObject();
+    writer.EndObject();
+
+    writer.StartObject();
+    writer.Key("id");
+    writer.String("l3");
+    writer.Key("from");
+    writer.String("n3");
+    writer.Key("to");
+    writer.String("n1");
+    writer.Key("style");
+    writer.StartObject();
+    writer.Key("fillColor");
+    writer.String("rgba(28,124,213,1)");
+    writer.Key("toDecoration");
+    writer.String("arrow");
+    writer.EndObject();
+    writer.EndObject();
+
+    writer.EndArray();
+    
+
+    writer.EndObject();
 
     response.headers().add<Http::Header::ContentType>(MIME(Application, Json));
+    response.headers().add<Http::Header::AccessControlAllowOrigin>("*");
     response.send(Http::Code::Ok, buf.GetString());
 }
 
-}
+}  // namespace Generic
 
 class StatsEndpoint {
-public:
+   public:
     explicit StatsEndpoint(Address addr)
-        : httpEndpoint(std::make_shared<Http::Endpoint>(addr))
-    { }
+        : httpEndpoint(std::make_shared<Http::Endpoint>(addr)) {}
 
     void init(size_t thr = 2) {
-        auto opts = Http::Endpoint::options()
-            .threads(static_cast<int>(thr));
+        auto opts = Http::Endpoint::options().threads(static_cast<int>(thr));
         httpEndpoint->init(opts);
         setupRoutes();
     }
@@ -73,24 +181,23 @@ public:
         httpEndpoint->serve();
     }
 
-private:
+   private:
     void setupRoutes() {
         using namespace Rest;
 
         Routes::Get(router, "/ready", Routes::bind(&Generic::handleReady));
-
     }
 
-  
-    using Lock = std::mutex;
+
+    using Lock  = std::mutex;
     using Guard = std::lock_guard<Lock>;
     Lock metricsLock;
 
     std::shared_ptr<Http::Endpoint> httpEndpoint;
-    Rest::Router router;
+    Rest::Router                    router;
 };
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
     Port port(9080);
 
     int thr = 2;
@@ -98,8 +205,7 @@ int main(int argc, char *argv[]) {
     if (argc >= 2) {
         port = static_cast<uint16_t>(std::stol(argv[1]));
 
-        if (argc == 3)
-            thr = std::stoi(argv[2]);
+        if (argc == 3) thr = std::stoi(argv[2]);
     }
 
     Address addr(Ipv4::any(), port);
