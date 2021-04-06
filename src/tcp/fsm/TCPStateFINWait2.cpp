653df4865fb6a1f23d7916ae82eb672626725c78
alright21 <alberto.giust@studenti.unitn.it>
2021-04-06 18:47:32 +0300

    feat(tcp): implement TCPStateFINWait2

--- src/tcp/fsm/TCPStateFINWait2.cpp
+++ src/tcp/fsm/TCPStateFINWait2.cpp
@@ -1,6 +1,9 @@
 #include "TCPStateFINWait2.h"
 
+#include <stack>
+
 #include "../../logger/Logger.h"
+#include "../TCPFlag.h"
 #include "TCPStateTimeWait.h"
 
 TCPStateFINWait2::TCPStateFINWait2(TCPStateMachine* stateMachine)
@@ -10,11 +13,21 @@ TCPStateFINWait2::TCPStateFINWait2(TCPStateMachine* stateMachine)
 }
 
 bool TCPStateFINWait2::onEvent(TCPEvent event) {
+    std::stack<pcpp::Layer*>* layers   = nullptr;
+    pcpp::TcpLayer*           tcpLayer = nullptr;
+
     bool handled = true;
     switch (event) {
         case TCPEvent::ReceiveFINSendACK:
             // The device receives a FIN from the other device. It acknowledges
             // it and moves to the TIME-WAIT state.
+            layers   = new std::stack<pcpp::Layer*>();
+            tcpLayer = craftTCPLayer(stateMachine->connection->srcPort,
+                                     stateMachine->connection->dstPort,
+                                     FIN + ACK);
+            layers->push(tcpLayer);
+            stateMachine->connection->owner->sendPacket(
+                layers, stateMachine->connection->dstAddr.toString());
             stateMachine->changeState(new TCPStateTimeWait(stateMachine));
 
             break;
