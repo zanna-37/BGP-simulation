30da8930570d2eb1a635eb2752ed1ecbcf6ff32b
alright21 <alberto.giust@studenti.unitn.it>
2021-04-07 08:46:05 +0300

    feat(endpoint): add info message for message dropping

--- src/entities/EndPoint.cpp
+++ src/entities/EndPoint.cpp
@@ -1,5 +1,7 @@
 #include "EndPoint.h"
 
+#include "../logger/Logger.h"
+
 void EndPoint::forwardMessage(stack<pcpp::Layer *> *layers,
                               NetworkCard *         networkcard) {
     // drop packet
