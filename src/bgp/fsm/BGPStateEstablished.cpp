4f6eed8f9989eebd866940031158fb08691baf98
Aurora <asdasdasdasd>
2021-11-02 10:58:38 +0100

    fix: deletes all routes associated with this connection

--- src/bgp/fsm/BGPStateEstablished.cpp
+++ src/bgp/fsm/BGPStateEstablished.cpp
@@ -51,13 +51,58 @@ bool BGPStateEstablished ::onEvent(BGPEvent event) {
                                 stateMachine->name,
                             stateMachine->connection->toString(),
                             "Sending NOTIFICATION message");
+
+                // Create BGPUpdateMessage (no PathAttributes)
+                pcpp::IPv4Address IPAddressPeer =
+                    stateMachine->connection->dstAddr;
+                pcpp::IPv4Address netMask;
+                // L_DEBUG("IPAddressPeer", IPAddressPeer.toString());
+                for (NetworkCard* networkCard :
+                     *stateMachine->connection->owner->networkCards) {
+                    // L_DEBUG("IP NETWORK CARD PEER",
+                    // networkCardPeer->IP.toString());
+                    if (networkCard->IP == stateMachine->connection->srcAddr) {
+                        netMask = networkCard->netmask;
+                        // L_DEBUG("IP NETCARD PEER",
+                        // netMaskPeer.toString());
+                    }
+                }
+                pcpp::IPv4Address networkIPpeer(IPAddressPeer.toInt() &
+                                                netMask.toInt());
+
+                std::vector<LengthAndIpPrefix> withdrawnRoutes;
+
+                uint8_t prefLenPeer =
+                    LengthAndIpPrefix::computeLengthIpPrefix(netMask);
+                LengthAndIpPrefix withdrawnRoute(prefLenPeer,
+                                                 networkIPpeer.toString());
+
+                withdrawnRoutes.push_back(withdrawnRoute);
+
+                std::vector<PathAttribute>     pathAttributes;
+                std::vector<LengthAndIpPrefix> nlris;
+                std::vector<uint16_t>          asPath;
+
+                std::unique_ptr<BGPUpdateLayer> bgpUpdateLayer =
+                    std::make_unique<BGPUpdateLayer>(
+                        withdrawnRoutes, pathAttributes, nlris);
+                bgpUpdateLayer->computeCalculateFields();
+
+                runDecisionProcess(stateMachine->connection->owner,
+                                   bgpUpdateLayer,
+                                   stateMachine->connection->srcAddr,
+                                   stateMachine->connection);
+
+                stateMachine->connection->bgpApplication->sendBGPUpdateMessage(
+                    stateMachine->connection,
+                    withdrawnRoutes,
+                    asPath,
+                    nlris,
+                    false);
             }
 
             // sets the ConnectRetryTimer to zero,
             stateMachine->resetConnectRetryTimer();
-
-            // TODO deletes all routes associated with this connection,
-            // TODO BGP table needed
 
             // XXX releases BGP resources, done
             stateMachine->connection->bgpApplication->stopListeningOnSocket(
@@ -570,9 +615,6 @@ bool BGPStateEstablished ::onEvent(BGPEvent event) {
                     nlris,
                     false);
             }
-
-            // TODO deletes all routes associated with this connection,
-            // bgp table needed
 
             // sets the ConnectRetryTimer to zero,
             stateMachine->resetConnectRetryTimer();
